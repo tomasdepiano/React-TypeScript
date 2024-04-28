a37a1a00d8ecabd0c15cd5b6da0fbe2ed32e296e
Tomas De Piano <tomasdepiano1@gmail.com>
2024-04-28 10:17:57 -0600

    Added use state to this project; made sure it was working properly with TypeScript and it works on site as well

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import CourseGoal from "./components/Goals.tsx";
 import Goals from "./assets/Goals.jpeg";
 import Header from "./components/Header.tsx";
+import { useState } from "react";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth!",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: Goals, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title="Learn TS">
-        <p>Look at this</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
