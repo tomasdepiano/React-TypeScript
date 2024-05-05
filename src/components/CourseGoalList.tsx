3953cd5a4aa44fa49a3dc9c065d1bcf083f40a73
Tomas De Piano <tomasdepiano1@gmail.com>
2024-05-05 16:07:46 -0600

    Added a warning box if the user adds too many goals and a box to render if no goals are added

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./Goals.tsx";
 import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
+import { type ReactNode } from "react";
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -10,15 +12,34 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You have no goals yet, start adding some!</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You are adding a lot of goals, don't put too much of your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
